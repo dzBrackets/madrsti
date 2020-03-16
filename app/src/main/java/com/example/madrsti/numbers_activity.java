0fe09a493d0df4f6f297684e3b10df747f82e945
Amine <62218221+Aminebob2206@users.noreply.github.com>
2020-03-16 23:01:43 +0100

    Amine_2.0

---
+++ app/src/main/java/com/example/madrsti/numbers_activity.java
@@ -0,0 +1,14 @@
+package com.example.madrsti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class numbers_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers_activity);
+    }
+}
